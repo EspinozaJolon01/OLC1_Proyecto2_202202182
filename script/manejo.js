56decb9a2b5303ef3e4e90b2d8b6a81501a9c5e4
EspinozaJolon01 <3524722860101@ingenieria.usac.edu.gt>
2024-04-02 22:08:36 -0600

    interface iniciada

---
+++ script/manejo.js
@@ -0,0 +1,21 @@
+function crearArchivos(){
+    console.log("vamos a crear un archivo");
+}
+
+
+function arbirArchivos(){
+    console.log("vamos abrir nuevos archviso")
+}
+
+function guardarArchivos(){
+    console.log("vamos a guardar")
+}
+
+function ejecutar(){
+    console.log("vamos a ejecutar")
+}
+
+
+function reportes(){
+    console.log("vamos a ver reportes")
+}
